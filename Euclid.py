ea5074cc7691017bde3dd7f49c81be689b2572df
yanerocket <rocketinterstellar@tutamail.com>
2025-12-04 15:45:17 +0300

    Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -2,6 +2,8 @@ def Euclid(num1, num2):
     if num1 == 0:
         return num2
     return Euclid(num2 % num1, num1)
+def main()
 number_1 = input("Введите первое число ")
 number_2 = input("Введите второе число ")
 print(f"Их наибольший общий делитель: {Euclid(int(number_1), int(number_2))}")
+
