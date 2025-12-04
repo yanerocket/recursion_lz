c4dfe59be980fc2a0ef651ba9c157e83bdae42fd
yanerocket <rocketinterstellar@tutamail.com>
2025-12-04 15:33:03 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+import Euclid
+import Packals_triangle
+import permutations
+import convert_to_base
+
+
+def main():
+    Euclid.main()
+    Packals_triangle.main()
+    permutations.main()
+    convert_to_base.main()
+
+if __name__ == '__main__':
+    main()
